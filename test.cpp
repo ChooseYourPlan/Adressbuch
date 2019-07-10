f5e02b5ed49df7538f4dfcef9c49cf4b70a33c6b
ChooseYourPlan <timweissenfels7@gmail.com>
2019-07-10 15:07:32 +0200

    Added cout_handler/Cout Testcase

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 
 TEST_CASE("Database initialisation") {
 
-database db("sample.txt");
+    database db("sample.txt");
     db.read();
 
     REQUIRE( db.get_vec_size() == 200);
@@ -40,10 +40,28 @@ database db("sample.txt");
 		REQUIRE_FALSE(db.get_ele(100) == buffer);
     }
 }
+
+template < class T >
+void cout_handler(const T& obj) {		
+		std::streambuf* cout_sbuf = std::cout.rdbuf(); 
+		std::ofstream fout("/dev/null");
+		std::cout.rdbuf(fout.rdbuf());
+		INFO("LOG:" << obj);
+		std::cout.rdbuf(cout_sbuf);
+}
+
+TEST_CASE("COUTS/PRINTS") {
+		database db("sample.txt");
+		db.read();
+		SECTION("COUTS", "[capture]") {
+				cout_handler(db[100]);
+		}
+}
+
 TEST_CASE("BENCHMARKS") {
 
-database db("sample.txt");
-db.read();
+	database db("sample.txt");
+    db.read();
 
     SECTION("INSERT/DELETE/CHANGED") {
 		entry ele1("Dummy","Data", "Element");
